b733c8d012942ae8a3baa8c21d2840104e3f5c22
Tharindu Thiwanka De Silva <chutte2012de@gmail.com>
2025-04-11 15:18:54 +0530

    Add Basic styling on stml stat result page

--- src/app/htmlstat/page.tsx
+++ src/app/htmlstat/page.tsx
@@ -14,12 +14,29 @@ export default async function Page({ params, searchParams }: any) {
   const htmlStatData = getHtmlStat(searchParams.inputUrl);
   const [htmlStat] = await Promise.all([htmlStatData]);
   return (
-    <div>
-      <div>Parsed JSON: {JSON.stringify(searchParams)}</div>
-      <div>URL: {htmlStat?.url}</div>
-      <div>Summary Id: {htmlStat?.summary_id}</div>
-      <div>Created At: {htmlStat?.created_at}</div>
-      <div>{JSON.stringify(htmlStat)}</div>
+    <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-3 p-4">
+      <div className="p-8 space-y-3">
+        <div className="tracking-wide text-lg text-indigo-500 font-semibold">
+          Input Web Page Url: {searchParams?.inputUrl}
+        </div>
+        <div className="block mt-1 text-sm leading-tight font-medium text-black">
+          Parsed JSON: {JSON.stringify(searchParams)}
+        </div>
+        <div className="block mt-1 text-sm leading-tight font-medium text-black">
+          URL: {htmlStat?.url}
+        </div>
+        <div className="block mt-1 text-sm leading-tight font-medium text-black">
+          Summary Id: {htmlStat?.summary_id}
+        </div>
+        <div className="block mt-1 text-sm leading-tight font-medium text-black">
+          Created At: {htmlStat?.created_at}
+        </div>
+        <div>Parsed JSON: {JSON.stringify(searchParams)}</div>
+        <div>URL: {htmlStat?.url}</div>
+        <div>Summary Id: {htmlStat?.summary_id}</div>
+        <div>Created At: {htmlStat?.created_at}</div>
+        <div>{JSON.stringify(htmlStat)}</div>
+      </div>
     </div>
   );
 }
