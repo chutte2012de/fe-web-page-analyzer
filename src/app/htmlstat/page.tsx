2b7049c82f96c691f98965206e13b9a563f071e1
Tharindu Thiwanka De Silva <chutte2012de@gmail.com>
2025-04-14 10:28:13 +0530

    Improve stuling in results page

--- src/app/htmlstat/page.tsx
+++ src/app/htmlstat/page.tsx
@@ -16,26 +16,36 @@ export default async function Page({ params, searchParams }: any) {
   return (
     <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-3 p-4">
       <div className="p-8 space-y-3">
-        <div className="tracking-wide text-lg text-indigo-500 font-semibold">
-          Input Web Page Url: {searchParams?.inputUrl}
+        <div className="rounded-3xl bg-cyan-300 py-2 md:py-4 ps-2 md:ps-4 pe-2 md:pe-4 font-sans text-xl">
+          {/* <div className="tracking-wide text-lg text-indigo-500 font-semibold">
+            Input Web Page Url: {searchParams?.inputUrl}
+          </div>
+          <div className="block mt-1 text-sm leading-tight font-medium text-black">
+            Parsed JSON: {JSON.stringify(searchParams)}
+          </div> */}
+          <div className="block mt-1 text-sm leading-tight font-medium text-black">
+            <strong>URL:</strong> {htmlStat?.url}
+          </div>
+          <div className="block mt-1 text-sm leading-tight font-medium text-black">
+            <strong>Request Id:</strong> {htmlStat?.id}
+          </div>
+          <div className="block mt-1 text-sm leading-tight font-medium text-black">
+            <strong>Title:</strong> {htmlStat?.title}
+          </div>
+          <div className="block mt-1 text-sm leading-tight font-medium text-black">
+            <strong>Version:</strong> {htmlStat?.version}
+          </div>
+          <div className="block mt-1 text-sm leading-tight font-medium text-black">
+            <strong>Created At:</strong> {htmlStat?.created_at}
+          </div>
         </div>
-        <div className="block mt-1 text-sm leading-tight font-medium text-black">
-          Parsed JSON: {JSON.stringify(searchParams)}
+        <div className="rounded-3xl bg-cyan-100 py-2 md:py-4 ps-2 md:ps-4 pe-2 md:pe-4 font-mono text-sm text-wrap">
+          <div>Parsed JSON: {JSON.stringify(searchParams)}</div>
+          <div>URL: {htmlStat?.url}</div>
+          <div>Summary Id: {htmlStat?.id}</div>
+          <div>Created At: {htmlStat?.created_at}</div>
+          <div>{JSON.stringify(htmlStat)}</div>
         </div>
-        <div className="block mt-1 text-sm leading-tight font-medium text-black">
-          URL: {htmlStat?.url}
-        </div>
-        <div className="block mt-1 text-sm leading-tight font-medium text-black">
-          Summary Id: {htmlStat?.summary_id}
-        </div>
-        <div className="block mt-1 text-sm leading-tight font-medium text-black">
-          Created At: {htmlStat?.created_at}
-        </div>
-        <div>Parsed JSON: {JSON.stringify(searchParams)}</div>
-        <div>URL: {htmlStat?.url}</div>
-        <div>Summary Id: {htmlStat?.summary_id}</div>
-        <div>Created At: {htmlStat?.created_at}</div>
-        <div>{JSON.stringify(htmlStat)}</div>
       </div>
     </div>
   );
